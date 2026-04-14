331f198ef649208da6969d51cfd5e0b7b182b14b
Quan <24521440@gm.uit.edu.vn>
2026-04-14 20:06:25 +0700

    upload project

---
+++ train_yolo11s.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo11s.pt")  # 🔥 đổi sang s (tốt hơn cho data nhỏ)
+
+model.train(
+    data="/kaggle/working/data.yaml",
+    epochs=100,
+    imgsz=960,
+    batch=8,
+
+    # augment
+    hsv_h=0.015,
+    hsv_s=0.7,
+    hsv_v=0.4,
+    degrees=10,
+    translate=0.1,
+    scale=0.5,
+    shear=2.0,
+    mosaic=1.0,
+
+    # 🔥 thêm mấy cái này
+    patience=20,     # tự dừng nếu không improve
+    save_period=1,   # lưu mỗi epoch (rất quan trọng)
+    name="train_yolo11"
+)
